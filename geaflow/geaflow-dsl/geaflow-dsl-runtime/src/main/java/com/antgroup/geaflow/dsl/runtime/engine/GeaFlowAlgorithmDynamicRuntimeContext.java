e39fea65c774ac37087f89c3af0e9460a5c6435f
Leomrlin <1229394985@qq.com>
2023-06-19 19:36:05 +0800

    [ISSUE-26] Support run user defined algorithm on dynamic graphs
    Signed-off-by: Leomrlin <1229394985@qq.com>
    
    * 支持动态图算法
    
    Author:    Leomrlin <1229394985@qq.com>
    Date:      Mon Jun 19 19:36:05 2023 +0800

---
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/main/java/com/antgroup/geaflow/dsl/runtime/engine/GeaFlowAlgorithmDynamicRuntimeContext.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2023 AntGroup CO., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package com.antgroup.geaflow.dsl.runtime.engine;
+
+import com.antgroup.geaflow.api.graph.function.vc.IncVertexCentricTraversalFunction.IncVertexCentricTraversalFuncContext;
+import com.antgroup.geaflow.api.graph.function.vc.IncVertexCentricTraversalFunction.TraversalGraphSnapShot;
+import com.antgroup.geaflow.api.graph.function.vc.VertexCentricTraversalFunction.TraversalEdgeQuery;
+import com.antgroup.geaflow.api.graph.function.vc.VertexCentricTraversalFunction.TraversalVertexQuery;
+import com.antgroup.geaflow.dsl.common.algo.AlgorithmRuntimeContext;
+import com.antgroup.geaflow.dsl.common.data.Row;
+import com.antgroup.geaflow.dsl.common.data.RowEdge;
+import com.antgroup.geaflow.dsl.common.exception.GeaFlowDSLException;
+import com.antgroup.geaflow.dsl.common.types.GraphSchema;
+import com.antgroup.geaflow.model.graph.edge.EdgeDirection;
+import com.antgroup.geaflow.model.graph.vertex.IVertex;
+import com.antgroup.geaflow.model.traversal.ITraversalResponse;
+import com.antgroup.geaflow.model.traversal.TraversalType.ResponseType;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class GeaFlowAlgorithmDynamicRuntimeContext implements AlgorithmRuntimeContext<Object, Object> {
+
+    private final IncVertexCentricTraversalFuncContext<Object, Row, Row, Object, Row> incVCTraversalCtx;
+
+    private final GraphSchema graphSchema;
+
+    protected TraversalVertexQuery<Object, Row> vertexQuery;
+
+    protected TraversalEdgeQuery<Object, Row> edgeQuery;
+
+    private Object vertexId;
+
+    private final Map<Object, Row> vertexId2NewValue = new HashMap<>();
+
+    public GeaFlowAlgorithmDynamicRuntimeContext(
+        IncVertexCentricTraversalFuncContext<Object, Row, Row, Object, Row> traversalContext,
+        GraphSchema graphSchema) {
+        this.incVCTraversalCtx = traversalContext;
+        this.graphSchema = graphSchema;
+        TraversalGraphSnapShot<Object, Row, Row> graphSnapShot = incVCTraversalCtx.getHistoricalGraph()
+            .getSnapShot(0L);
+        this.vertexQuery = graphSnapShot.vertex();
+        this.edgeQuery = graphSnapShot.edges();
+    }
+
+    public void setVertexId(Object vertexId) {
+        this.vertexId = vertexId;
+        this.vertexQuery.withId(vertexId);
+        this.edgeQuery.withId(vertexId);
+    }
+
+    public Iterator<Object> loadAllVertex() {
+        return vertexQuery.loadIdIterator();
+    }
+
+    public IVertex loadVertex() {
+        return vertexQuery.get();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<RowEdge> loadEdges(EdgeDirection direction) {
+        switch (direction) {
+            case OUT:
+                return (List) edgeQuery.getOutEdges();
+            case IN:
+                return (List) edgeQuery.getInEdges();
+            case BOTH:
+                List<RowEdge> edges = new ArrayList<>();
+                edges.addAll((List) edgeQuery.getOutEdges());
+                edges.addAll((List) edgeQuery.getInEdges());
+                return edges;
+            default:
+                throw new GeaFlowDSLException("Illegal edge direction: " + direction);
+        }
+    }
+
+    @Override
+    public void sendMessage(Object vertexId, Object message) {
+        incVCTraversalCtx.sendMessage(vertexId, message);
+    }
+
+    @Override
+    public void updateVertexValue(Row value) {
+        vertexId2NewValue.put(vertexId, value);
+    }
+
+    @Override
+    public void take(Row row) {
+        incVCTraversalCtx.takeResponse(new AlgorithmResponse(row));
+    }
+
+    public Row getVertexNewValue() {
+        return vertexId2NewValue.get(vertexId);
+    }
+
+    public long getCurrentIterationId() {
+        return incVCTraversalCtx.getIterationId();
+    }
+
+    @Override
+    public GraphSchema getGraphSchema() {
+        return graphSchema;
+    }
+
+    private static class AlgorithmResponse implements ITraversalResponse<Row> {
+
+        private final Row row;
+
+        public AlgorithmResponse(Row row) {
+            this.row = row;
+        }
+
+        @Override
+        public long getResponseId() {
+            return 0;
+        }
+
+        @Override
+        public Row getResponse() {
+            return row;
+        }
+
+        @Override
+        public ResponseType getType() {
+            return ResponseType.Vertex;
+        }
+    }
+}
---
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/main/java/com/antgroup/geaflow/dsl/runtime/engine/GeaFlowAlgorithmDynamicTraversalFunction.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2023 AntGroup CO., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package com.antgroup.geaflow.dsl.runtime.engine;
+
+import com.antgroup.geaflow.api.function.iterator.RichIteratorFunction;
+import com.antgroup.geaflow.api.graph.function.vc.IncVertexCentricTraversalFunction;
+import com.antgroup.geaflow.dsl.common.algo.AlgorithmUserFunction;
+import com.antgroup.geaflow.dsl.common.data.Row;
+import com.antgroup.geaflow.dsl.common.data.RowVertex;
+import com.antgroup.geaflow.dsl.common.types.GraphSchema;
+import com.antgroup.geaflow.model.graph.edge.IEdge;
+import com.antgroup.geaflow.model.graph.vertex.IVertex;
+import com.antgroup.geaflow.model.traversal.ITraversalRequest;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+public class GeaFlowAlgorithmDynamicTraversalFunction
+    implements IncVertexCentricTraversalFunction<Object, Row, Row, Object, Row>, RichIteratorFunction {
+
+    private static final long VERSION = 0L;
+
+    private final AlgorithmUserFunction<Object, Object> userFunction;
+
+    private final Object[] params;
+
+    private GraphSchema graphSchema;
+
+    private IncVertexCentricTraversalFuncContext<Object, Row, Row, Object, Row> traversalContext;
+
+    private GeaFlowAlgorithmDynamicRuntimeContext algorithmCtx;
+
+    private MutableGraph<Object, Row, Row> mutableGraph;
+
+    public GeaFlowAlgorithmDynamicTraversalFunction(GraphSchema graphSchema,
+                                                    AlgorithmUserFunction<Object, Object> userFunction,
+                                                    Object[] params) {
+        this.graphSchema = Objects.requireNonNull(graphSchema);
+        this.userFunction = Objects.requireNonNull(userFunction);
+        this.params = Objects.requireNonNull(params);
+    }
+
+    @Override
+    public void open(IncVertexCentricTraversalFuncContext<Object, Row, Row, Object, Row> traversalContext) {
+        this.algorithmCtx = new GeaFlowAlgorithmDynamicRuntimeContext(traversalContext, graphSchema);
+        this.userFunction.init(algorithmCtx, params);
+        this.mutableGraph = traversalContext.getMutableGraph();
+        this.traversalContext = traversalContext;
+    }
+
+    @Override
+    public void init(ITraversalRequest<Object> traversalRequest) {
+        Object vertexId = traversalRequest.getVId();
+        algorithmCtx.setVertexId(vertexId);
+        RowVertex vertex = (RowVertex) algorithmCtx.loadVertex();
+        algorithmCtx.setVertexId(vertex.getId());
+        userFunction.process(vertex, Collections.emptyIterator());
+    }
+
+    @Override
+    public void evolve(Object vertexId, TemporaryGraph<Object, Row, Row> temporaryGraph) {
+        IVertex<Object, Row> vertex = temporaryGraph.getVertex();
+        if (vertex != null) {
+            mutableGraph.addVertex(VERSION, vertex);
+        }
+        List<IEdge<Object, Row>> edges = temporaryGraph.getEdges();
+        if (edges != null) {
+            for (IEdge<Object, Row> edge : edges) {
+                mutableGraph.addEdge(VERSION, edge);
+            }
+        }
+    }
+
+    @Override
+    public void compute(Object vertexId, Iterator<Object> messages) {
+        algorithmCtx.setVertexId(vertexId);
+        RowVertex vertex = (RowVertex) algorithmCtx.loadVertex();
+        Row newValue = algorithmCtx.getVertexNewValue();
+        if (newValue != null) {
+            vertex.setValue(newValue);
+        }
+        userFunction.process(vertex, messages);
+    }
+
+    @Override
+    public void finish(Object vertexId, MutableGraph<Object, Row, Row> mutableGraph) {
+    }
+
+    @Override
+    public void initIteration(long windowId) {
+    }
+
+    @Override
+    public void finishIteration(long windowId) {
+    }
+}
