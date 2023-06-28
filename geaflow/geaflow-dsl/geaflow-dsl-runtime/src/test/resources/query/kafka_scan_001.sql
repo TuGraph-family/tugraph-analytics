3527a894c8cd30cc2dd84c89ba467403c5c39cfc
Leomrlin <1229394985@qq.com>
2023-06-28 13:38:19 +0800

    [ISSUE-48] Fix kafka start time parse exception

--- geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/query/kafka_scan_001.sql
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/query/kafka_scan_001.sql
@@ -4,7 +4,8 @@ CREATE TABLE kafka_source (
 ) WITH (
 	type='kafka',
 	geaflow.dsl.kafka.servers = 'localhost:9092',
-	geaflow.dsl.kafka.topic = 'sink-test'
+	geaflow.dsl.kafka.topic = 'sink-test',
+  `geaflow.dsl.start.time` = '2025-10-01 09:00:00'
 );
 
 CREATE TABLE tbl_result (
