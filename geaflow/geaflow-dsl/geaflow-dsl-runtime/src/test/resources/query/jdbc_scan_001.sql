7d8bbfe2a7b87364acc424a4b6071ea211e7218a
Draco <dracode01@gmail.com>
2023-07-04 00:15:07 +0800

    Fix tab in JDBC test

--- geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/query/jdbc_scan_001.sql
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/query/jdbc_scan_001.sql
@@ -3,11 +3,11 @@ CREATE TABLE jdbc_source (
 	user_count bigint
 ) WITH (
 	type='jdbc',
-    geaflow.dsl.jdbc.driver = 'org.h2.Driver',
-    geaflow.dsl.jdbc.url = 'jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1',
-    geaflow.dsl.jdbc.username = 'h2_user',
-    geaflow.dsl.jdbc.password = 'h2_pwd',
-    geaflow.dsl.jdbc.table.name = 'test'
+	geaflow.dsl.jdbc.driver = 'org.h2.Driver',
+	geaflow.dsl.jdbc.url = 'jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1',
+	geaflow.dsl.jdbc.username = 'h2_user',
+	geaflow.dsl.jdbc.password = 'h2_pwd',
+	geaflow.dsl.jdbc.table.name = 'test'
 );
 
 CREATE TABLE tbl_result (
--- geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/query/jdbc_write_001.sql
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/query/jdbc_write_001.sql
@@ -5,9 +5,9 @@ CREATE TABLE tbl_result (
 	type='jdbc',
 	geaflow.dsl.jdbc.driver = 'org.h2.Driver',
 	geaflow.dsl.jdbc.url = 'jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1',
-    geaflow.dsl.jdbc.username = 'h2_user',
-    geaflow.dsl.jdbc.password = 'h2_pwd',
-    geaflow.dsl.jdbc.table.name = 'test'
+	geaflow.dsl.jdbc.username = 'h2_user',
+	geaflow.dsl.jdbc.password = 'h2_pwd',
+	geaflow.dsl.jdbc.table.name = 'test'
 );
 
 INSERT INTO tbl_result VALUES ('json', 111)
