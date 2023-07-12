bb0de14f9d0ea5e4180c47dca64860cd8c5b2d95
Leomrlin <1229394985@qq.com>
2023-07-12 19:10:07 +0800

    [ISSUE-81] Add SNB LDBC BI tests for DSL

---
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/ldbc/bi_02.sql
@@ -0,0 +1,181 @@
+CREATE GRAPH bi (
+  --static
+  --Place
+  Vertex Country (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  Vertex City (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  Vertex Continent (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  --Organisation
+  Vertex Company (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  Vertex University (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  --Tag
+	Vertex TagClass (
+	  id bigint ID,
+	  name varchar,
+	  url varchar
+	),
+	Vertex Tag (
+	  id bigint ID,
+	  name varchar,
+	  url varchar
+	),
+
+  --dynamic
+  Vertex Person (
+    id bigint ID,
+    creationDate bigint,
+    firstName varchar,
+    lastName varchar,
+    gender varchar,
+    --birthday Date,
+    --email {varchar},
+    --speaks {varchar},
+    browserUsed varchar,
+    locationIP varchar
+  ),
+  Vertex Forum (
+    id bigint ID,
+    creationDate bigint,
+    title varchar
+  ),
+  --Message
+  Vertex Post (
+    id bigint ID,
+    creationDate bigint,
+    browserUsed varchar,
+    locationIP varchar,
+    content varchar,
+    length bigint,
+    lang varchar,
+    imageFile varchar
+  ),
+  Vertex Comment (
+    id bigint ID,
+    creationDate bigint,
+    browserUsed varchar,
+    locationIP varchar,
+    content varchar,
+    length bigint
+  ),
+
+  --relations
+  --static
+	Edge isLocatedIn (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge isPartOf (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+  Edge isSubclassOf (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+  Edge hasType (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+
+  --dynamic
+	Edge hasModerator (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge containerOf (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge replyOf (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge hasTag (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+  Edge hasInterest (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+  Edge hasCreator (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+  Edge workAt (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    workForm bigint
+  ),
+  Edge studyAt (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    classYear bigint
+  ),
+
+  --temporary
+  Edge hasMember (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    creationDate bigint
+  ),
+  Edge likes (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    creationDate bigint
+  ),
+  Edge knows (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    creationDate bigint
+  )
+) WITH (
+ 	storeType='rocksdb'
+);
+
+USE GRAPH bi;
+
+CREATE TABLE tbl_result (
+  name string,
+  countWindow1 bigint,
+  countWindow2 bigint,
+  diff bigint
+) WITH (
+	type='file',
+	geaflow.dsl.file.path='${target}'
+);
+
+INSERT INTO tbl_result
+SELECT name, countWindow1, countWindow2, abs(countWindow2 - countWindow1) as diff
+FROM (
+MATCH (tagClass:TagClass where name = 'Comedian')<-[:hasType]-(tag)
+    , (msg:Post|Comment where creationDate between 1672502400000 and 1672502400000 + 17280000000)
+      -[:hasTag]->(tag)
+RETURN  tag.name as name,
+        COUNT(if (msg.creationDate between 1672502400000
+        and 1672502400000 + 8640000000, 1, cast(null as int))) as countWindow1,
+        COUNT(if (msg.creationDate between 1672502400000 + 8640000001
+        and 1672502400000 + 17280000000, 1, cast(null as int))) as countWindow2
+GROUP BY tag.name
+)
+ORDER BY diff desc, name LIMIT 100
+;
---
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/test/resources/ldbc/bi_15.sql
@@ -0,0 +1,173 @@
+CREATE FUNCTION bi15 AS 'com.antgroup.geaflow.dsl.udf.ldbc.BIConnectionPathAlgorithm';
+
+CREATE GRAPH bi (
+  --static
+  --Place
+  Vertex Country (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  Vertex City (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  Vertex Continent (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  --Organisation
+  Vertex Company (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  Vertex University (
+    id bigint ID,
+    name varchar,
+    url varchar
+  ),
+  --Tag
+	Vertex TagClass (
+	  id bigint ID,
+	  name varchar,
+	  url varchar
+	),
+	Vertex Tag (
+	  id bigint ID,
+	  name varchar,
+	  url varchar
+	),
+
+  --dynamic
+  Vertex Person (
+    id bigint ID,
+    creationDate bigint,
+    firstName varchar,
+    lastName varchar,
+    gender varchar,
+    --birthday Date,
+    --email {varchar},
+    --speaks {varchar},
+    browserUsed varchar,
+    locationIP varchar
+  ),
+  Vertex Forum (
+    id bigint ID,
+    creationDate bigint,
+    title varchar
+  ),
+  --Message
+  Vertex Post (
+    id bigint ID,
+    creationDate bigint,
+    browserUsed varchar,
+    locationIP varchar,
+    content varchar,
+    length bigint,
+    lang varchar,
+    imageFile varchar
+  ),
+  Vertex Comment (
+    id bigint ID,
+    creationDate bigint,
+    browserUsed varchar,
+    locationIP varchar,
+    content varchar,
+    length bigint
+  ),
+
+  --relations
+  --static
+	Edge isLocatedIn (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge isPartOf (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+  Edge isSubclassOf (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+  Edge hasType (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+
+  --dynamic
+	Edge hasModerator (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge containerOf (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge replyOf (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+	Edge hasTag (
+	  srcId bigint SOURCE ID,
+	  targetId bigint DESTINATION ID
+	),
+  Edge hasInterest (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+  Edge hasCreator (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID
+  ),
+  Edge workAt (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    workForm bigint
+  ),
+  Edge studyAt (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    classYear bigint
+  ),
+
+  --temporary
+  Edge hasMember (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    creationDate bigint
+  ),
+  Edge likes (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    creationDate bigint
+  ),
+  Edge knows (
+    srcId bigint SOURCE ID,
+    targetId bigint DESTINATION ID,
+    creationDate bigint
+  )
+) WITH (
+  	storeType='rocksdb'
+ );
+
+USE GRAPH bi;
+
+CREATE TABLE tbl_result (
+  weight double
+) WITH (
+	type='file',
+	geaflow.dsl.file.path='${target}'
+);
+
+--GQL
+INSERT INTO tbl_result
+SELECT case when weight > 99999999 then -1.0 else ROUND(weight, 2) end
+FROM (
+  CALL bi15(1100006, 1100007, 1600000000000, 1700000000000) YIELD (distance)
+  RETURN MIN(distance) as weight
+)
+;
