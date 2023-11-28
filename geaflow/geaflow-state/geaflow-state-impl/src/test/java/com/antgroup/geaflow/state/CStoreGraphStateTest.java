27c4421e152df854cd3407ff46fc121b8652d8d0
Leomrlin <1229394985@qq.com>
2023-11-28 19:47:02 +0800

    [ISSUE-281] Add cstore module

--- geaflow/geaflow-state/geaflow-state-impl/src/test/java/com/antgroup/geaflow/state/CStoreGraphStateTest.java
+++ geaflow/geaflow-state/geaflow-state-impl/src/test/java/com/antgroup/geaflow/state/CStoreGraphStateTest.java
@@ -62,10 +62,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-@Ignore
 public class CStoreGraphStateTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(CStoreGraphStateTest.class);
 
