fc3518d48cefec06bf4355704520399fc89a9d70
liukaiming-alipay <lkm01250564@antgroup.com>
2024-10-30 15:10:36 +0800

    feat: remove i18

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -37,14 +37,6 @@ const config: Config = {
     },
   },
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
-
   presets: [
     [
       "classic",
