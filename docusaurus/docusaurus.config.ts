5ab4557a2620b5434d3d65c247cae67d8ce82081
liukaiming-alipay <lkm01250564@antgroup.com>
2024-10-30 11:11:30 +0800

    feat: page config

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://tugraph.doc",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/tugraph-analytics/",
