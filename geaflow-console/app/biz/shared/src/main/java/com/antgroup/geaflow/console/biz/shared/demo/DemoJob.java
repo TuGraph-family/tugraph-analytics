97bbb8659894fc461a547ba804d6cf24fea494cc
lfs <1836214581@qq.com>
2024-11-19 12:38:05 +0800

    [ISSUE-377] Fix demo job and auto create demo job when console starts

---
+++ geaflow-console/app/biz/shared/src/main/java/com/antgroup/geaflow/console/biz/shared/demo/DemoJob.java
@@ -0,0 +1,8 @@
+package com.antgroup.geaflow.console.biz.shared.demo;
+
+import com.antgroup.geaflow.console.core.model.job.GeaflowJob;
+
+public abstract class DemoJob {
+
+    public abstract GeaflowJob build();
+}
