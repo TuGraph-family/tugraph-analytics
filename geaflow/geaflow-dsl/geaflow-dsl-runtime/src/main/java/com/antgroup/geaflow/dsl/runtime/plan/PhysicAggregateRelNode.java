3129270150491aafc54a8f597450fd72aa8a9217
yaozhongq <yaozhqp@gmail.com>
2025-05-15 14:03:33 +0800

    feat: support stddev_samp

--- geaflow/geaflow-dsl/geaflow-dsl-runtime/src/main/java/com/antgroup/geaflow/dsl/runtime/plan/PhysicAggregateRelNode.java
+++ geaflow/geaflow-dsl/geaflow-dsl-runtime/src/main/java/com/antgroup/geaflow/dsl/runtime/plan/PhysicAggregateRelNode.java
@@ -52,6 +52,9 @@ import com.antgroup.geaflow.dsl.udf.table.agg.MinBinaryString;
 import com.antgroup.geaflow.dsl.udf.table.agg.MinDouble;
 import com.antgroup.geaflow.dsl.udf.table.agg.MinInteger;
 import com.antgroup.geaflow.dsl.udf.table.agg.MinLong;
+import com.antgroup.geaflow.dsl.udf.table.agg.StdDevSampDouble;
+import com.antgroup.geaflow.dsl.udf.table.agg.StdDevSampInteger;
+import com.antgroup.geaflow.dsl.udf.table.agg.StdDevSampLong;
 import com.antgroup.geaflow.dsl.udf.table.agg.SumDouble;
 import com.antgroup.geaflow.dsl.udf.table.agg.SumInteger;
 import com.antgroup.geaflow.dsl.udf.table.agg.SumLong;
@@ -91,6 +94,8 @@ public class PhysicAggregateRelNode extends Aggregate implements PhysicRelNode<R
     public static final String UDAF_MAX = "MAX";
 
     public static final String UDAF_MIN = "MIN";
+
+    public static final String UDAF_STDDEV_SAMP = "STDDEV_SAMP";
 
     public PhysicAggregateRelNode(
         RelOptCluster cluster,
@@ -434,6 +439,11 @@ public class PhysicAggregateRelNode extends Aggregate implements PhysicRelNode<R
                     aggClasses.add(MinDouble.class);
                     aggClasses.add(MinInteger.class);
                     aggClasses.add(MinBinaryString.class);
+                    break;
+                case UDAF_STDDEV_SAMP:
+                    aggClasses.add(StdDevSampLong.class);
+                    aggClasses.add(StdDevSampDouble.class);
+                    aggClasses.add(StdDevSampInteger.class);
                     break;
                 default:
                     throw new GeaFlowDSLException("Not support aggregate function " + aggName);
