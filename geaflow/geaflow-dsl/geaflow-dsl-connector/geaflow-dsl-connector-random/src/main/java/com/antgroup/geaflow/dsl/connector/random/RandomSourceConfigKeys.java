219723e8ea79f4905dacdd21a524214fcf363d6d
Leomrlin <1229394985@qq.com>
2025-07-10 16:48:36 +0800

    minor fix

--- geaflow/geaflow-dsl/geaflow-dsl-connector/geaflow-dsl-connector-random/src/main/java/com/antgroup/geaflow/dsl/connector/random/RandomSourceConfigKeys.java
+++ geaflow/geaflow-dsl/geaflow-dsl-connector/geaflow-dsl-connector-random/src/main/java/com/antgroup/geaflow/dsl/connector/random/RandomSourceConfigKeys.java
@@ -25,7 +25,7 @@ import com.antgroup.geaflow.common.config.ConfigKeys;
 public class RandomSourceConfigKeys {
 
     public static final ConfigKey GEAFLOW_DSL_RANDOM_SOURCE_RATE = ConfigKeys
-        .key("geaflow.dsl.random.source.tuples.per.second.")
+        .key("geaflow.dsl.random.source.tuples.per.second")
         .defaultValue(1.0)
         .description("Random source create tuple number per second");
 
